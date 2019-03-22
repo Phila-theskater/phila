1fb1f62c934f3c1c5670962ed1ed56f0c5f3601a
Phila-theskater <48821126+Phila-theskater@users.noreply.github.com>
2019-03-22 18:53:56 +0200

    Update sorting.py

--- mypackage/sorting.py
+++ mypackage/sorting.py
@@ -1,10 +1,12 @@
 def bubble_sort(alist):
+    
     return alist.sort()
    
-
 def merge_sort(alist):
+    
     return alist.sort()
 def quick_sort(alist):
-   return alist.sort()
+    
+    return alist.sort()
 
 
