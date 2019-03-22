68e4fd79e6c25286fb36ffe088ef8d78f924e647
Phila-theskater <48821126+Phila-theskater@users.noreply.github.com>
2019-03-22 18:57:09 +0200

    Update recursion.py

--- mypackage/recursion.py
+++ mypackage/recursion.py
@@ -1,28 +1,27 @@
 def sum_array(array):
-
+     
+     
+     
      '''Return sum of all items in array'''
-    n = array
-    l = array[1:]
-    if len(n)>0:
-        return  array[0] + sum_array(l)
-    else :
-        return 0
+     n = array
+     l = array[1:]
+     if len(n)>0:
+          return  array[0] + sum_array(l)
+     else :
+          return 0
 
 def fibonacci(n):
-
-    '''Return nth term in fibonacci sequence'''
+     '''Return nth term in fibonacci sequence'''
      if n <= 1:
-        return n
-
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+          return n
+     else:
+          
+          return fibonacci(n - 1) + fibonacci(n - 2)
 
 def reverse(word):
-
-    n = word
-    l= word[:-1]
-    if len(n)>0:
-        return  n[-1] + str(reverse(l))
-    else :
-
-        return ''
+     n = word
+     l= word[:-1]
+     if len(n)>0:
+          return  n[-1] + str(reverse(l))
+     else :
+          return ''
